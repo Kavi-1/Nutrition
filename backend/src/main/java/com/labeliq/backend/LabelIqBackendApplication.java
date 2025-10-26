19acf15654de51c0ba746e4b7378ae5a417129a7
YS-05 <yash23sharma05@gmail.com>
2025-10-26 17:51:34 -0400

    Initial commit, set up Java Spring Boot backend

---
+++ backend/src/main/java/com/labeliq/backend/LabelIqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.labeliq.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelIqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabelIqBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/labeliq/backend/LabelIqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labeliq.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelIqBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
